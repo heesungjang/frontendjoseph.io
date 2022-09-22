1bca1a6dcc67060d63ae6f56c992390641543177
heesungjang <heesungj7@gmail.com>
2022-09-22 19:37:53 +1000

    svg config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     domains: ['localhost', '*'],
   },
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    });
+    return config;
+  },
   swcMinify: true,
 };
 
