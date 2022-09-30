f41a313362ce44325caf3744f68cef3e5c8c6895
heesungjang <heesungj7@gmail.com>
2022-09-30 13:03:47 +1000

    og title 추가

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -45,11 +45,7 @@ export default function Post({ page, blocks, frontmatter }) {
         <title>{`${frontmatter.title[0].text.content} | ${page.properties.Name.title[0].plain_text}`}</title>
         <meta
           property="og:title"
-          content={`Heelog ${
-            +page.properties.Name.title[0].plain_text
-              ? ' | ' + page.properties.Name.title[0].plain_text
-              : ''
-          }`}
+          content={`Heelog | ${page.properties.Name.title[0].plain_text}`}
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
