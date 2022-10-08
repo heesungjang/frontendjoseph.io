8de73cdaeb87c12f9f1884324571d350adf25bed
heesungjang <heesungj7@gmail.com>
2022-10-09 01:54:18 +1000

    fix static sitemap generation config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@ const siteUrl = 'https://heelog.dev';
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'],
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
     additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
