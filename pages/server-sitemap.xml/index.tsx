045d323c522edf57cbfb3999f93aaf9c1668e1a3
heesungjang <heesungj7@gmail.com>
2022-10-14 19:47:12 +1000

    🚧 두번째 refactor 컴포넌트 구조 단순화 및 타입 정리

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -1,9 +1,10 @@
 import { GetServerSideProps } from 'next';
 import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
-import { databaseId } from '..';
+
 import { fetchPosts } from '../../lib/notions';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const databaseId = process.env.NOTION_DATABASE_ID;
   const { posts } = await fetchPosts(databaseId!);
   const siteUrl = 'https://heelog.dev';
 
