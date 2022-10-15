765c864a2a8b807e11d935363591215d81cab10e
heesungjang <heesungj7@gmail.com>
2022-10-16 05:15:11 +1000

    link to vercel

---
+++ components/Laypout.js
@@ -0,0 +1,78 @@
+// 1. 유지보수
+// 2. git --> mySever -->
+
+import { todoAPI } from './api';
+
+//3. 모듈화가 먼지 모르는게 보여요
+
+const Layout = () => {
+  const addTodo = (todo) => {
+    todoAPI.addTodo();
+  };
+
+  return <div onClick={() => addTodo({ title: 'heesung' })}></div>;
+};
+
+const Layout = () => {
+  const addTodo = (todo) => {
+    todoAPI.addTodo();
+  };
+
+  return <div onClick={() => addTodo({ title: 'heesung' })}></div>;
+};
+
+const Layout = () => {
+  const addTodo = (todo) => {
+    todoAPI.addTodo();
+  };
+
+  return <div onClick={() => addTodo({ title: 'heesung' })}></div>;
+};
+
+const Layout = () => {
+  const addTodo = (todo) => {
+    todoAPI.addTodo();
+  };
+
+  return <div onClick={() => addTodo({ title: 'heesung' })}></div>;
+};
+
+const Layout = () => {
+  const addTodo = (todo) => {
+    todoAPI.addTodo();
+  };
+
+  return <div onClick={() => addTodo({ title: 'heesung' })}></div>;
+};
+
+const Layout = () => {
+  const addTodo = (todo) => {
+    todoAPI.addTodo();
+  };
+
+  return <div onClick={() => addTodo({ title: 'heesung' })}></div>;
+};
+
+const Layout = () => {
+  const addTodo = (todo) => {
+    todoAPI.addTodo();
+  };
+
+  return <div onClick={() => addTodo({ title: 'heesung' })}></div>;
+};
+
+const Layout = () => {
+  const addTodo = (todo) => {
+    todoAPI.addTodo();
+  };
+
+  return <div onClick={() => addTodo({ title: 'heesung' })}></div>;
+};
+
+const Layout = () => {
+  const addTodo = (todo) => {
+    todoAPI.addTodo();
+  };
+
+  return <div onClick={() => addTodo({ title: 'heesung' })}></div>;
+};
---
+++ pages/test.jsx
@@ -0,0 +1,24 @@
+//
+router('myServer/todos', (res, req) => {
+  const id = req.id;
+  const { title: '', content: '' } = req.body.data;
+
+  if(title){
+      // -->
+  // [1]      [2]    [3]    [4]
+  // [title][asd][asd][asd]
+  }
+  if(content){
+      // -->
+  // [1]      [2]    [3]    [4]
+  // [title][asd][asd][asd]
+  }
+
+});
+
+{
+    title:"
+    "
+}
+
+axios.put('myServer/todo/id', title);
