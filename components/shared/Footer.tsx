54b5ac8fb1552d48031dca3bec68f33c3faea06e
heesungjang <heesungj7@gmail.com>
2022-11-08 21:36:44 +1000

    re-brand blog name from heelog --> frontend joseph

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 const Footer: React.FC = () => {
   return (
     <FooterWrapper>
-      <FooterText>Copyright © Heesung Jang</FooterText>
+      <FooterText>Copyright © Joseph Jang</FooterText>
     </FooterWrapper>
   );
 };
