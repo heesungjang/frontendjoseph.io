dea2e17dc266368ebca017cedc0ec7a5d16cd12d
heesungjang <heesungj7@gmail.com>
2023-03-04 13:52:16 +1000

    added linkedin link

--- slug.ts
+++ slug.ts
@@ -1,9 +1,9 @@
 const GITHUB_LINK = 'https://github.com/heesungjang';
 const EMAIL_ADDRESS = 'heesungj7@gmail.com';
-// const LINKEDIN_LINK = 'https://github.com/heesungjang';
+const LINKEDIN_LINK = 'https://www.linkedin.com/in/joseph-jang-4975a5239';
 
 export const socialLinks = {
   gitHub: GITHUB_LINK,
+  linkedIn: LINKEDIN_LINK,
   email: EMAIL_ADDRESS,
-  // linkedIn: LINKEDIN_LINK,
 };
